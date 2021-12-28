0e663aef27099a234fa40b7ba78f40fb7778fb0d
Karen Kwok <kkwok98@hotmail.com>
2021-12-28 18:39:57 -0500

    added fontawesome

--- frontend/src/Water/WaterTrack/WaterTrack.js
+++ frontend/src/Water/WaterTrack/WaterTrack.js
@@ -1,4 +1,6 @@
 import "./WaterTrack.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus, faSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function WaterTrack() {
   return <div>WaterTrack</div>;
