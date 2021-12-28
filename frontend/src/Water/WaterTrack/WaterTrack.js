93fc60d644d788692a6d57eff9eed20d84410180
Karen Kwok <kkwok98@hotmail.com>
2021-12-28 18:41:25 -0500

    added content to watertrack

--- frontend/src/Water/WaterTrack/WaterTrack.js
+++ frontend/src/Water/WaterTrack/WaterTrack.js
@@ -3,5 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus, faSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function WaterTrack() {
-  return <div>WaterTrack</div>;
+  return (
+    <div id="watertrack-body">
+      <div id="streak">🔥 Streak: 503 days</div>
+      <div id="icons">
+        <div class="plus-minus">
+          <FontAwesomeIcon icon={faMinus} />
+        </div>
+        <div id="cup">
+          <FontAwesomeIcon icon={faSquare} />
+        </div>
+        <div class="plus-minus">
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+      </div>
+      <div id="progress">
+        <div id="num-cups">4/8</div>
+        <div id="bar-percentage">
+          <div id="bar"></div>
+          <div id="percentage">50%</div>
+        </div>
+        <div id="reset">Reset</div>
+      </div>
+    </div>
+  );
 }
