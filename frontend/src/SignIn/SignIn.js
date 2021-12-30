377dbf07872a142ad63ed6d40ebb59275164b150
Jason Yuan <yuanjason98@gmail.com>
2021-12-30 18:27:35 -0500

    connected to login api

--- frontend/src/SignIn/SignIn.js
+++ frontend/src/SignIn/SignIn.js
@@ -1,8 +1,22 @@
-import "./SignIn.scss";
-import React from "react";
+import { post } from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setUser } from "../features/auth/authSlice";
+import "./SignIn.scss";
 
 function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const onSignInFormSubmit = () => {
+    // Calls the thunk action creator, and passes the thunk function to dispatch
+    dispatch(login(username, password));
+  };
+  const onUsernameChange = (event) => setUsername(event.target.value);
+  const onPasswordChange = (event) => setPassword(event.target.value);
+
   return (
     <div id="signin-body">
       <div id="signin-box">
@@ -12,6 +26,7 @@ function SignIn() {
             class="signin-input"
             type="text"
             placeholder="Username"
+            onChange={onUsernameChange}
           ></input>
         </div>
         <div>
@@ -19,10 +34,11 @@ function SignIn() {
             class="signin-input"
             type="password"
             placeholder="Password"
+            onChange={onPasswordChange}
           ></input>
         </div>
         <div>
-          <button id="signin-submit" type="submit">
+          <button id="signin-submit" type="submit" onClick={onSignInFormSubmit}>
             Sign In
           </button>
         </div>
@@ -35,3 +51,15 @@ function SignIn() {
 }
 
 export default SignIn;
+
+// createUser is the "thunk action creator"
+function login(username, password) {
+  // createUserThunk is the "thunk function"
+  return async function createLogin(dispatch, getState) {
+    const response = await post(`http://localhost:8000/auth/signin/`, {
+      username,
+      password,
+    });
+    dispatch(setUser(response.data));
+  };
+}
