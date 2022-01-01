e1277a7b19d8d73e6381408c3a26a375e0880793
Jason Yuan <yuanjason98@gmail.com>
2022-01-01 14:17:44 -0500

    navigate user to water track if they are authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,53 @@
+import { defaults, get } from 'axios';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.scss";
-import SignIn from "./SignIn/SignIn";
-import SignUp from "./SignUp/SignUp";
+import { setUser } from "./features/auth/authSlice";
 import Header from "./Header/Header";
 import { Home } from "./Home/Home";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
 import { Water } from "./Water/Water";
+import { WaterLeaderboard } from "./Water/WaterLeaderboard/WaterLeaderboard";
 import { WaterTrack } from "./Water/WaterTrack/WaterTrack";
 import { WaterTrends } from "./Water/WaterTrends/WaterTrends";
-import { WaterLeaderboard } from "./Water/WaterLeaderboard/WaterLeaderboard";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { defaults, get } from 'axios';
-import { setUser } from "./features/auth/authSlice";
-import { useDispatch } from "react-redux";
 
 function App() {
-  const dispatch = useDispatch();
-
   // make axios client save cookies
   defaults.withCredentials = true;
 
-  // get the user information through the session cookie
-  dispatch(getUserActionCreator());
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const username = useSelector((state) => state.auth.user ? state.auth.user.username : null);
+
+  useEffect(() => {
+    // get the user information through the session cookie
+    dispatch(getUserActionCreator());
+
+    if (username !== null) {
+      // there is a valid user navigate them to the water track page
+      console.log('navigate them to the water track page')
+      navigate('/water/track');
+    } else {
+      navigate('/signin');
+    }
+  }, [username]);
 
   return (
-    <Router>
+    <div>
       <Header></Header>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-        <Routes>
+      <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/water" element={<Water />}>
             <Route path="/water/track" element={<WaterTrack />} />
             <Route path="/water/trends" element={<WaterTrends />} />
             <Route path="/water/leaderboard" element={<WaterLeaderboard />} />
           </Route>
         </Routes>
-      </div>
-    </Router>
+    </div>
   );
 }
 
