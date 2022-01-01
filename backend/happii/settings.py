f5e0500e0e191a3da81db66bd7487d47c543a804
Jason Yuan <yuanjason98@gmail.com>
2022-01-01 17:53:58 -0500

    added water app to installed apps

--- backend/happii/settings.py
+++ backend/happii/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'happii.water.apps.WaterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
