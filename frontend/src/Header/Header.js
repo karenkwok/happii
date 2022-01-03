dc64797174c983c12af558931d923ddf11a037a9
Karen Kwok <kkwok98@hotmail.com>
2022-01-03 16:53:53 -0500

    made date dynamic

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -3,15 +3,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const today = new Date();
+  const dd = String(today.getDate());
+  const year = today.getFullYear();
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan. ",
+    "Feb. ",
+    "Mar. ",
+    "Apr. ",
+    "May ",
+    "Jun. ",
+    "Jul. ",
+    "Aug. ",
+    "Sep. ",
+    "Oct. ",
+    "Nov. ",
+    "Dec. ",
+  ];
+
+  const day = days[today.getDay()];
+  const month = months[today.getMonth()];
+
   return (
     <header>
       <div id="left-header">
-        <div id="logo"><Link to="/">happii</Link></div>
-        <div id="section"><Link to="/water">Water</Link></div>
+        <div id="logo">
+          <Link to="/">happii</Link>
+        </div>
+        <div id="section">
+          <Link to="/water">Water</Link>
+        </div>
         <div id="date">
-          <div id="weekday">Saturday</div>
-          <div id="month-day">Jan. 1</div>
-          <div id="year">2022</div>
+          <div id="weekday">{day}</div>
+          <div id="month-day">
+            {month}
+            {dd}
+          </div>
+          <div id="year">{year}</div>
         </div>
       </div>
       <div id="right-header">
