1640f6e95834eacfd2a80ef4e460db1fd7d219b8
Karen Kwok <kkwok98@hotmail.com>
2022-01-04 23:36:22 -0500

    changed hardcoded text to variables

--- frontend/src/Water/WaterTrack/WaterTrack.js
+++ frontend/src/Water/WaterTrack/WaterTrack.js
@@ -3,9 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus, faSquare } from "@fortawesome/free-solid-svg-icons";
 
 export function WaterTrack() {
+  const streak = "🔥 Streak: 503 days";
+  const numCups = "4/8";
+  const percentage = "50%";
+
   return (
     <div id="watertrack-body">
-      <div id="streak">🔥 Streak: 503 days</div>
+      <div id="streak">{streak}</div>
       <div id="icons">
         <div class="plus-minus">
           <FontAwesomeIcon icon={faMinus} />
@@ -18,10 +22,10 @@ export function WaterTrack() {
         </div>
       </div>
       <div id="progress">
-        <div id="num-cups">4/8</div>
+        <div id="num-cups">{numCups}</div>
         <div id="bar-percentage">
           <div id="bar"></div>
-          <div id="percentage">50%</div>
+          <div id="percentage">{percentage}</div>
         </div>
         <div id="reset">Reset</div>
       </div>
