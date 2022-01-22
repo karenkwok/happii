b08c17fd172740ae7cce849445005cfef1f46329
Jason Yuan <yuanjason98@gmail.com>
2022-01-22 10:45:32 -0500

    implemented water streak API

--- backend/happii/water/urls.py
+++ backend/happii/water/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('intake/', views.WaterIntake.as_view(), name='intake'),
     path('intake_trends/', views.WaterIntakeTrends.as_view(), name='intake_trends'),
+    path('streak/', views.WaterStreak.as_view(), name='streak'),
 ]
