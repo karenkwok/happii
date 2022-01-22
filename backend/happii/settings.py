3b0e49d8928bb8c6def4d746cbd3a271cad11340
Jason Yuan <yuanjason98@gmail.com>
2022-01-22 13:41:06 -0500

    allowed options requests to bypass authentication

--- backend/happii/settings.py
+++ backend/happii/settings.py
@@ -169,6 +169,6 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'happii.auth.permissions.IsAuthenticatedOrReadOnly',
     ],
 }
