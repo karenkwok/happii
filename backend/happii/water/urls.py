1b440385c9015e7a0a75f15a89c1cc110eb5ab0e
Jason Yuan <yuanjason98@gmail.com>
2022-01-22 12:38:40 -0500

    implemented water goal API

--- backend/happii/water/urls.py
+++ backend/happii/water/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("intake/", views.WaterIntake.as_view(), name="intake"),
     path("intake_trends/", views.WaterIntakeTrends.as_view(), name="intake_trends"),
     path("streak/", views.WaterStreak.as_view(), name="streak"),
+    path("goal/", views.WaterGoal.as_view(), name="goal"),
 ]
--- backend/happii/water/views.py
+++ backend/happii/water/views.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.db.models import Q
 from happii.water.models import Intake
 from rest_framework import status
 from rest_framework.response import Response
@@ -66,3 +67,38 @@ class WaterStreak(APIView):
         today = datetime.date.today()
         delta = today - latest_date_of_broken_streak.date
         return Response(delta.days)
+
+
+class WaterGoal(APIView):
+    @staticmethod
+    def get(request):
+        date_period = request.query_params.get("date_period")
+
+        filter = Q(user=request.user, intake__gte=8)
+
+        if date_period is None:
+            return Response(
+                "Please provide a date period", status=status.HTTP_400_BAD_REQUEST
+            )
+        elif date_period == "weekly":
+            now = datetime.datetime.now()
+            # start of the week
+            monday = now - datetime.timedelta(days=now.weekday())
+            # end of the week
+            sunday = monday + datetime.timedelta(days=6)
+
+            goals_completed = Intake.objects.filter(
+                filter,
+                date__range=[monday, sunday],
+            ).count()
+            return Response(goals_completed)
+        elif date_period == "monthly":
+            # get the current month
+            month = datetime.date.today().month
+            goals_completed = Intake.objects.filter(filter, date__month=month).count()
+            return Response(goals_completed)
+        else:
+            return Response(
+                "Provided date period is invalid, must be either weekly or monthly",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
