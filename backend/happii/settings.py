79a9d5373543fd03f0fd67076a414ba1baf74c72
Jason Yuan <yuanjason98@gmail.com>
2022-01-22 11:13:45 -0500

    format backend

--- backend/happii/settings.py
+++ backend/happii/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tlp+_3%m%sntbs&-@b5jc8k%j6+2!0hp-j^%k!%b_n1sq#*9gl'
+SECRET_KEY = "django-insecure-tlp+_3%m%sntbs&-@b5jc8k%j6+2!0hp-j^%k!%b_n1sq#*9gl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,60 +31,60 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'happii.water.apps.WaterConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
+    "happii.water.apps.WaterConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 if DEBUG == True:
-    MIDDLEWARE.append('happii.middleware.corsmiddleware.CORSMiddleware')
+    MIDDLEWARE.append("happii.middleware.corsmiddleware.CORSMiddleware")
 
-ROOT_URLCONF = 'happii.urls'
+ROOT_URLCONF = "happii.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'happii.wsgi.application'
+WSGI_APPLICATION = "happii.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'happii',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "happii",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -94,16 +94,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -125,19 +125,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # CSRF Cookie
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#csrf-cookie-samesite
 
-CSRF_COOKIE_SAMESITE = 'Strict'
+CSRF_COOKIE_SAMESITE = "Strict"
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#csrf-cookie-secure
 if DEBUG == False:
@@ -151,7 +151,7 @@ SESSION_COOKIE_HTTPONLY = True
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#session-cookie-samesite
 
-SESSION_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SAMESITE = "Strict"
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#session-cookie-secure
 
@@ -162,10 +162,10 @@ if DEBUG == False:
 # https://www.django-rest-framework.org/api-guide/settings/
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
--- backend/happii/water/views.py
+++ backend/happii/water/views.py
@@ -7,39 +7,51 @@ from happii.water.models import Intake
 
 # Create your views here.
 
+
 class WaterIntakeTrends(APIView):
     @staticmethod
     def get(request):
-        start_date = request.query_params.get('start_date')
-        end_date = request.query_params.get('end_date')
-        
-        response = Intake.objects.filter(
-            date__range=[start_date, end_date], user_id=request.user).values().order_by("date")
-        return Response(response)
+        start_date = request.query_params.get("start_date")
+        end_date = request.query_params.get("end_date")
 
+        response = (
+            Intake.objects.filter(
+                date__range=[start_date, end_date], user_id=request.user
+            )
+            .values()
+            .order_by("date")
+        )
+        return Response(response)
 
 
 class WaterIntake(APIView):
     @staticmethod
     def get(request):
-        date = request.query_params.get('date')
+        date = request.query_params.get("date")
 
         try:
-            response = Intake.objects.get(
-                date=date, user=request.user).intake
+            response = Intake.objects.get(date=date, user=request.user).intake
             return Response(response)
         except Intake.DoesNotExist:
             return Response(0)
 
     @staticmethod
     def put(request):
-        date = request.query_params.get('date')
-        intake = request.data.get('intake')
+        date = request.query_params.get("date")
+        intake = request.data.get("intake")
 
         if date is None or intake is None:
-            return Response("Please provide date and intake.", status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                "Please provide date and intake.", status=status.HTTP_400_BAD_REQUEST
+            )
 
         (obj, created) = Intake.objects.update_or_create(
-            user=request.user, date=date,
-            defaults={'intake': intake, })
-        return Response({'intake': obj.intake, 'date': obj.date}, status=status.HTTP_200_OK)
+            user=request.user,
+            date=date,
+            defaults={
+                "intake": intake,
+            },
+        )
+        return Response(
+            {"intake": obj.intake, "date": obj.date}, status=status.HTTP_200_OK
+        )
